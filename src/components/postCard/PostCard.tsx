4fe010af3e717221c48afecbebdf53466080f07e
Milad Joodi <winbeta.com@gmail.com>
2024-07-26 02:42:12 +0330

    Update PostCard.tsx

--- src/components/postCard/PostCard.tsx
+++ src/components/postCard/PostCard.tsx
@@ -1,7 +1,7 @@
 import { PostProps } from "@/types/types";
 import React from "react";
 
-const PostCard = ({title,body}: PostProps) => {
+const PostCard = ({ title, body }: PostProps) => {
   return (
     <div className="postCard">
       <h1>{title}</h1>
