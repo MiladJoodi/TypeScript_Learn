19815448eb362aacaea58e0ea228732adf6fd400
Milad Joodi <winbeta.com@gmail.com>
2024-07-28 14:03:54 +0330

    Update page.tsx

--- src/app/4-usestate-example/page.tsx
+++ src/app/4-usestate-example/page.tsx
@@ -2,16 +2,40 @@
 
 import { useState } from "react";
 
+type UserType = {
+  name: string;
+  sessionId: number;
+}
+
 const UseStateExample = () => {
-  const [username, setUsername] = useState();
-  const [user, setUser] = useState();
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState<UserType | null>();
+
+  // input change
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+
+  // onclick button
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setUser({
+      name: username,
+      sessionId: Math.random()
+    })
+  }
 
   return (
     <div className="useStateExample">
-      <form>
-        <input type="text" placeholder="Username" />
-        <button>Login</button>
+      {user ? (
+        `${user.name} loggend in`
+      ): (
+        <form>
+        <input type="text" placeholder="Username" onChange={handleChange} />
+        <button onClick={handleClick}>Login</button>
       </form>
+      )}
+      
     </div>
   );
 }
