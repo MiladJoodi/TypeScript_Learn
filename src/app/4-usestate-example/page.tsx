5078d8575173c7b94eab8e0f3d5542ec2c7171fb
Milad Joodi <winbeta.com@gmail.com>
2024-07-28 13:50:05 +0330

    Update page.tsx

--- src/app/4-usestate-example/page.tsx
+++ src/app/4-usestate-example/page.tsx
@@ -1,42 +1,19 @@
-"use client";
-import React, { useState } from "react";
+"use client"
 
-type UserType = {
-  sessionId: number;
-  name: string;
-};
+import { useState } from "react";
 
 const UseStateExample = () => {
-  const [username, setUsername] = useState("");
-  const [user, setUser] = useState<UserType | null>(null);
-  // OR
-  // const [user, setUser] = useState<UserType>();
+  const [username, setUsername] = useState();
+  const [user, setUser] = useState();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setUser({
-      name: username,
-      sessionId: Math.random(),
-    });
-  };
   return (
     <div className="useStateExample">
-      {user ? (
-        `${user.name} logged in`
-      ) : (
-        <form>
-          <input type="text" placeholder="Username" onChange={handleChange} />
-          <button onClick={handleClick}>Login</button>
-        </form>
-      )}
-      {/* BE AWARE */}
-      {user?.name}
+      <form>
+        <input type="text" placeholder="Username" />
+        <button>Login</button>
+      </form>
     </div>
   );
-};
+}
 
 export default UseStateExample;
